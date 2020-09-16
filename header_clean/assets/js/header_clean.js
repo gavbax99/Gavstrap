c2448ef1932ec38be5b1031f7f3aa05f26f64dd3
Gavin <gav.bax@gmail.com>
2020-09-16 14:32:05 -0700

    updated header-clean

--- header_clean/assets/js/header_clean.js
+++ header_clean/assets/js/header_clean.js
@@ -1,30 +1,36 @@
-// ==================== Height of nav
-var navHeight = parseFloat($("header").css("height"));
+// HEIGHT OF NAV
+const navHeight = parseFloat($("header").css("height"));
 $(".header-offset").css("height", navHeight)
 
 
 // SCROLL BUTTONS
-
 $(".header-nav__scroll").on("click", function () {
-	var data = $(this).attr("data-name");
+	const data = $(this).attr("data-name");
 	$('html,body').animate({ scrollTop: $(`#${data}`).offset().top - navHeight }, 400);
 });
 
 // HAMBURGER
-
 $(".header-hamburger").on("click", function () {
 	$(this).toggleClass("hamburger-change");
 	$(".header-hamburger-nav").toggleClass("header-hamburger-nav--show");
-	$(".header-hamburger__blackout").toggleClass("header-hamburger__blackout--show")
+	$(".header-hamburger__blackout").toggleClass("header-hamburger__blackout--show");
 });
 
 $(".header-nav__scroll").on("click", function () {
 	$(".header-hamburger").removeClass("hamburger-change");
 	$(".header-hamburger-nav").removeClass("header-hamburger-nav--show");
-	$(".header-hamburger__blackout").removeClass("header-hamburger__blackout--show")
+	$(".header-hamburger__blackout").removeClass("header-hamburger__blackout--show");
 });
 
 $(".header-hamburger--expand").on("click", function () {
 	$(this).toggleClass("header-hamburger--carat-flip");
 	$(".header-hamburger__list__nested").toggleClass("hider");
 });
+
+$(".header-hamburger__blackout").on("click", function () {
+	$(".header-hamburger").toggleClass("hamburger-change");
+	$(".header-hamburger-nav").toggleClass("header-hamburger-nav--show");
+	$(".header-hamburger__blackout").toggleClass("header-hamburger__blackout--show");
+});
+
+
--- header_clean/index.php
+++ header_clean/index.php
@@ -8,12 +8,32 @@
 <!-- ================================================ -->
 
 <!-- Remove `wrapper` for full-width -->
-<div class="flex-full wrapper home-wrapper">
+<div id="home-wrapper" class="flex-full flex-col">
 <!-- ================================================ -->
 
+	<div class="row">
+		<div class="c-sm-12 c-md-6 c-xl-3 test-col">1</div>
+		<div class="c-sm-12 c-md-6 c-xl-3 test-col">2</div>
+		<div class="c-sm-12 c-md-6 c-xl-3 test-col">3</div>
+		<div class="c-sm-12 c-md-6 c-xl-3 test-col">4</div>
+	</div>
 
-	<!-- STUFF -->
+	<div class="row">
+		<div class="c-sm-6 c-md-2 c-xl-1 test-col">1</div>
+		<div class="c-sm-6 c-md-2 c-xl-1 test-col">2</div>
+		<div class="c-sm-6 c-md-2 c-xl-1 test-col">3</div>
+		<div class="c-sm-6 c-md-2 c-xl-1 test-col">4</div>
+		<div class="c-sm-6 c-md-2 c-xl-1 test-col">5</div>
+		<div class="c-sm-6 c-md-2 c-xl-1 test-col">6</div>
+		<div class="c-sm-6 c-md-2 c-xl-1 test-col">7</div>
+		<div class="c-sm-6 c-md-2 c-xl-1 test-col">8</div>
+		<div class="c-sm-6 c-md-2 c-xl-1 test-col">9</div>
+		<div class="c-sm-6 c-md-2 c-xl-1 test-col">10</div>
+		<div class="c-sm-6 c-md-2 c-xl-1 test-col">11</div>
+		<div class="c-sm-fill c-md-2 c-xl-1 test-col">12</div>
+	</div>
 
+</div>
 
 <!-- ================================================ -->
 </div>
